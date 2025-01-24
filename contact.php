07a4ac3dc37ce59f2cf33a1803c110444bb6f319
jyotisarokar <109975405+jyotisarokar@users.noreply.github.com>
2025-01-24 16:29:13 +0530

    24-01-2025 16:28
    Jyoti Sarokar

--- contact.php
+++ contact.php
@@ -1,21 +1,42 @@
-<?php
-$nm = isset($_POST['nm']) ? htmlspecialchars($_POST['nm']) : '';
-$cm = isset($_POST['cm']) ? htmlspecialchars($_POST['cm']) : '';
-$em = isset($_POST['em']) ? htmlspecialchars($_POST['em']) : '';
-$subject = isset($_POST['subject']) ? htmlspecialchars($_POST['subject']) : '';
-$msg = isset($_POST['msg']) ? htmlspecialchars($_POST['msg']) : '';
 
-$to = "jyotisarok8@gmail.com";
-$email_subject = "Message from " . $nm . " - " . $subject;
+
+<?php
+
+ 
+   $nm=isset($_POST['nm']) ? $_POST['nm']: ' ';
+  
+     $em=isset($_POST['em']) ? $_POST['em']: ' ';
+     $cm=isset($_POST['cm']) ? $_POST['cm']: ' ';
+     $subject=isset($_POST['subject']) ? $_POST['subject']: ' ';
+     $msg=isset($_POST['msg']) ? $_POST['msg']: ' ';
+
+$to = "ashwinisitsolutions@gmail.com";
+$subject = "Message from Website";
+ 
+ 
 
 $txt = "Name = ". $nm .  "\r\n Email =" . $em . "\r\n Company= " . $cm . "\r\n Message =" . $msg . "\r\n Subject =" . $subject . "\r\n";
 
-$headers = "From: " . $em; 
+$headers ="From :ashwinisitsolutions@gmail.com"; 
 
-if(mail($to, $email_subject, $txt, $headers)) {
-    header("Location: thanku.html");
-    exit();
-} else {
+
+
+if(mail($to, $subject, $txt, $headers))
+{
+    echo "Mail sent successfully";
+}
+
+else 
+{   
     echo "Error sending mail";
 }
+
+  function function_alert($txt) {
+
+   echo "<script>alert('$txt');</script>";
+}
+   header("Location: thanku.html");
+   
+
+
 ?>
